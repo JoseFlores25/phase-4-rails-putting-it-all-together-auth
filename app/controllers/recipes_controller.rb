302d7fae3055a50307c2c70cacee532824bfef1e
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-12 19:17:39 -0500

    Done

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,32 @@
+class RecipesController < ApplicationController
+  before_action :authorize
+
+  def index
+
+      recipes = Recipe.all
+      render json: recipes
+  end
+
+  def create
+      recipe = Recipe.create(user_id: session[:user_id], title:recipe_params[:title], instructions:recipe_params[:instructions], minutes_to_complete:recipe_params[:minutes_to_complete])
+      if recipe.valid?
+          render json: recipe, status: :created
+      else
+          render json: { errors: recipe.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+
+  private
+
+  def recipe_params
+      params.permit(:title, :instructions, :minutes_to_complete,)
+  end
+
+  def authorize
+      return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  has_many :recipes
+
+  has_secure_passwordue
+end
